7e3ba5258eefab7e136e1a12a8a753f4ddc6571e
BlueDog <gopaaj@gmail.com>
2021-09-19 00:22:29 +0600

    0.2

--- Script/StickingWoll2D.cs
+++ Script/StickingWoll2D.cs
@@ -9,29 +9,27 @@ public class StickingWoll2D : MonoBehaviour
     public PhysicMaterial Material;
     private Rigidbody2D RB;
     private Vector3 vector;
-    private PhysicMaterial DefauldMaterial;
+    private PhysicsMaterial2D DefauldMaterial;
     bool Flag;
     void Start()
     {
         //RB = gameObject.GetComponent<Rigidbody>();
         vector = Vector3.zero;
-        DefauldMaterial = GetComponent<Collider>().material;
+        DefauldMaterial = GetComponent<Collider2D>().sharedMaterial;
     }
-    void OnCollisionEnter(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
         Flag = true;
         RB = collision.rigidbody;
         vector = -gameObject.transform.up;
         collision.rigidbody.angularDrag = this.AngularDrag;
         //collision.collider.material = Material;
-        
     }
-    void OnCollisionExit(Collision collision)
+    void OnCollisionExit2D(Collision2D collision)
     {
         Flag = false;
         collision.rigidbody.angularDrag = 0.05f;
-        collision.collider.material = DefauldMaterial;  
+        collision.collider.sharedMaterial = DefauldMaterial;  
     }
 
     void FixedUpdate()
